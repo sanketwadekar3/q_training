8ab6399d0a5868834bcefb4946212ae8c55dc496
Sanket Wadekar <46665854+sanketwadekar3@users.noreply.github.com>
2020-08-18 19:44:17 +0530

    Update test_cal.py

--- test_cal.py
+++ test_cal.py
@@ -11,28 +11,28 @@ class TestCal(unittest.TestCase):
         Test that the addition of two integers returns the correct data
         """
         result = cal.cal_fun(1,2,"Addition")
-        self.assertEqual(result,"3")
+        self.assertEqual(result,3)
 
     def test_sub(self):
         """
         Test that the subtraction of two integers returns the correct data
         """
         result = cal.cal_fun(5,3,"Subtraction")
-        self.assertEqual(result,"2")
+        self.assertEqual(result,2)
 
     def test_mul(self):
         """
         Test that the multiplication of two integers returns the correct data
         """
         result = cal.cal_fun(2,3,"Multiplication")
-        self.assertEqual(result,"6")
+        self.assertEqual(result,6)
 
     def test_div(self):
         """
         Test that the division of two integers returns the correct data
         """
         result = cal.cal_fun(6,3,"Division")
-        self.assertEqual(result,"2")
+        self.assertEqual(result,2.0)
 
 if __name__ == '__main__':
     unittest.main()
